612b3036c19163872370a475e60513bb8f92becb
bellapacx <bellapacx@gmail.com>
2025-07-18 19:14:47 +0300

    hja

--- src/components/DashboardScreen.jsx
+++ src/components/DashboardScreen.jsx
@@ -1050,18 +1050,19 @@ const togglePlayPause = () => {
 
   return (
     <div
-      key={num}
-      className={`py-2 rounded-lg font-bold text-2xl transition-all duration-200 shadow-lg flex items-center justify-center cursor-pointer hover:scale-110 hover:ring-4 hover:ring-yellow-300/40 hover:z-10
-        ${isCurrent
-          ? 'bg-gradient-to-br from-yellow-300 via-yellow-400 to-amber-400 text-yellow-900 font-extrabold transform scale-110 ring-4 ring-yellow-300 animate-blink shadow-yellow-300/60 shadow-2xl drop-shadow-lg'
-          : isCalled
-            ? 'bg-yellow-700/100 text-yellow-900 border-yellow-300 shadow-yellow-200/40 border font-bold'
-            : `${categoryColors[letter]} border drop-shadow-md`
-        }`}
-      style={{ height: '50px' }}
-    >
-      {num.toString().padStart(2, '0')}
-    </div>
+  key={num}
+  className={`py-2 rounded-lg font-bold text-2xl transition-all duration-200 shadow-lg flex items-center justify-center cursor-pointer hover:scale-110 hover:ring-4 hover:ring-yellow-300/40 hover:z-10
+    ${isCurrent
+      ? 'bg-gradient-to-br from-yellow-300 via-yellow-400 to-amber-400 text-yellow-900 font-extrabold transform scale-110 ring-4 ring-yellow-300 animate-blink shadow-yellow-300/60 shadow-2xl drop-shadow-lg'
+      : isCalled
+        ? 'bg-yellow-500 text-white border-yellow-300 shadow-md shadow-yellow-200/40 font-bold'
+        : `${categoryColors[letter]} border drop-shadow-md`
+    }`}
+  style={{ height: '50px' }}
+>
+  {num.toString().padStart(2, '0')}
+</div>
+
   );
 })}
 
