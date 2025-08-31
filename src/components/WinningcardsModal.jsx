d5e319657ad0e24a5acd0922d187eb44e9f0cfa1
sunvilacoin <sunvilacoin@gmail.com>
2025-08-31 20:47:53 +0300

    Fresh startddsdsssdddaddddkhsskk

--- src/components/WinningcardsModal.jsx
+++ src/components/WinningcardsModal.jsx
@@ -177,7 +177,7 @@ const playCheckSound = () => {
                         onClick={() => handleMarkAsChecked(card.card_id)}
                         className="mb-4 px-4 py-2 bg-red-600 hover:bg-red-700 rounded text-white font-semibold transition"
                       >
-                        Mark as Checked
+                        እሰር
                       </button>
                     )}
 
